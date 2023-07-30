79c920faa0370240214b47b8b60daaa81bca9bb5
Ridhika Srivastava <125787653+rr8461@users.noreply.github.com>
2023-07-30 15:58:01 +0530

    Delete voice_assistance directory

--- voice_assistance/voice_as.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-import wikipedia
-voice=pyttsx3.init()
-input_1 = input("Search here: ")
-result=wikipedia.summary(input_1,sentences=3)
-print(result)
-voice.say(result)
-voice.runAndWait()
